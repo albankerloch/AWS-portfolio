b4560bd518dce12fc080632a455377e99b44e8e2
EC2 Default User <ec2-user@ip-172-31-25-123.eu-west-3.compute.internal>
2024-01-29 15:03:13 +0000

    use proxy for api

--- api/add_contact/app.py
+++ api/add_contact/app.py
@@ -8,6 +8,14 @@ from __future__ import print_function
 import boto3
 import os
 import time
+import logging
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+patch_all()
+
 
 dynamoDBResource = boto3.resource('dynamodb')
 
@@ -16,16 +24,37 @@ def lambda_handler(event, context):
     # Log debug information
     print(event)
     
-    # Extracting the user parameters from the event
-    Mail = event["Mail"]
-    Nom = event["Nom"]
-    Message = event['Message']
+    # create the response object, the error code is 500 unless manually set to a success
+    response = {
+        'isBase64Encoded': False,
+        'statusCode': 500,
+        'body': '',
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+    
     ddbTable = os.environ['TABLE_NAME']
     
-    # DynamoDB 'put_item' to add or update a note
-    newNoteId = upsertItem(dynamoDBResource, ddbTable, Mail, Nom, Message)
+    try:
+        # Extracting the user parameters from the event and environment
+        params = extractParams(event)
 
-    return newNoteId
+        # Add X-Ray annotations to the trace
+        add_annotation(params['Mail'], params['Nom'], params['Message'])
+
+        # DynamoDB 'put_item' to add or update a note
+        newNoteId = upsertItem(dynamoDBResource, ddbTable, params['Mail'], params['Nom'], params['Message'])
+
+    except Exception as e:
+        print(e)
+        response['body'] = e
+        return response
+        
+    response['statusCode'] = 200
+    response['body'] = str(newNoteId)
+    return response
 
 def upsertItem(dynamoDBResource, ddbTable, Mail, Nom, Message):
     print('upsertItem Function')
@@ -44,3 +73,26 @@ def upsertItem(dynamoDBResource, ddbTable, Mail, Nom, Message):
         }
     )
     return Mail
+    
+def add_annotation(Mail, Nom, Message):
+    print('add_annotation Function')
+    xray_recorder.begin_subsegment('Add a contact')
+    xray_recorder.put_annotation("Mail", Mail)
+    xray_recorder.put_annotation("Nom", Nom)
+    xray_recorder.put_annotation("Message", Message)
+    xray_recorder.end_subsegment()
+
+
+
+def extractParams(event):
+    print('extractParams Function')
+    print(event["body"])
+    Mail = "MAIL TEST"
+    Nom = "NOM TEST"
+    Message = "MESSAGE TEST"
+    
+    return {
+        'Mail': Mail,
+        'Nom': Nom,
+        'Message': Message
+    }
